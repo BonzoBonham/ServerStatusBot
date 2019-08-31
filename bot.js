d2c5313f271f30349c7a78bc588fbe23e3c0cc4c
Nicolas <pereznicolas90@gmail.com>
2019-08-31 18:23:25 -0500

    addserver and removeserver are functional!

--- bot.js
+++ bot.js
@@ -1,6 +1,5 @@
 const Discord = require("discord.js");
 const Gamedig = require("gamedig");
-//const fs = require("fs");
 const mongoose = require("mongoose");
 const Guild = require("./schemas.js");
 const { token } = require("./temp.json");
@@ -8,12 +7,16 @@ const { token } = require("./temp.json");
 //Create bot instance
 const bot = new Discord.Client({ disableEveryone: true });
 
+//fix mongoose warnings
+mongoose.set('useFindAndModify', false);
+//mongoose.set('debug', true);
+
 //connect to status bot DB, handle connection errors
 mongoose.connect('mongodb://localhost/statusbot', { useNewUrlParser: true });
 var db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error:'));
 db.once('open', function () {
-    console.log("I'M IN!")
+    console.log("Connected to MongoDB!")
     Guild.find({ name: "SSS" }).exec((err, data) => { console.log(JSON.stringify(data)) }) //test query
 });
 
@@ -85,6 +88,10 @@ const vChannelUpdate = function (channelid, server) {
         .catch(console.error);
 }
 
+const pushServer = function (server) {
+
+}
+
 //function that takes STARTUP_MESSAGE, a channel ID and a server object, and edits the 
 //MUST BE CALLED AFTER QUERY
 const tChannelUpdate = (message, channelid, server) =>
@@ -131,7 +138,7 @@ const guildQuery = function () {
 }
 
 //message handler for commands
-const handleMessage = message => {
+const handleMessage = async message => {
     if (
         message === undefined || // Message must exist
         message.author.bot || // Message must not be from the bot
@@ -150,16 +157,120 @@ const handleMessage = message => {
             .map(code => getPrefix() + code.toLowerCase())
             .find(code => code === cmd.toLowerCase())
     ) {
-        console.log("unrecognized command entered. I will ignore it 8)");
+        console.log("Unrecognized command entered. I will ignore it 8)");
         //message.channel.send("Sorry! We didn't recognize that command.");
     }
 
+    //command that displays the credits for the bot
     if (cmd === `${getPrefix()}${COMMANDS.CREDITS}`) {
+        message.channel.send(
+            //Credits: Bonzo hehe
+            "I was made by <@164216955367063552>"
+        );
+    }
+
+    //command that DM's the help for the bot to the user
+    if (cmd === `${getPrefix()}${COMMANDS.HELP}`) {
         message.channel.send(
             "I was made by Bonzo"
         );
     }
+
+    //
+    if (cmd === `${getPrefix()}${COMMANDS.ADD_SERVER}`) {
+        //MESSAGE PROTOTYPE: !addserver dmgttt garrysmod 1.1.1.1
+
+        //3 arguments must be provided
+        if (args.length != 3) { message.channel.send("Error: Invalid number of parameters"); return; }
+        message.channel.send("Initializing server tracking...");
+
+        //extract info from arguments
+        let serverName = args[0];
+        let gameType = args[1];
+        let serverIP = args[2];
+
+        //create text and voice channels
+        let tChannelName = serverName + "-player-list";
+        let tChannelID;
+        let vChannelID;
+        let currentGuild = message.guild;
+        let currentGuildID = currentGuild.id;
+
+        //create text channel
+        await currentGuild.createChannel(tChannelName, { type: "text" })
+            .then(textchan => {
+                tChannelID = textchan.id;
+            })
+            .catch(() => console.log('Failed to create text channel'))
+
+        //create voice channel
+        await currentGuild.createChannel("Initializing", { type: "voice" })
+            .then(voicechan => {
+                vChannelID = voicechan.id;
+            })
+            .catch(() => console.log('Failed to create voice channel'))
+
+        //create server object to be pushed to server array
+        var newServer = { name: serverName, type: gameType, host: serverIP, vchannelid: vChannelID, tchannelid: tChannelID }
+
+        //add server to database array
+        console.log("Updating servers info...");
+        Guild.findOneAndUpdate({ discordid: currentGuildID }, { $push: { servers: newServer } })
+            .then(console.log("Info updated and uploaded!"))
+            .catch(console.error)
+
+    }
+
+    if (cmd === `${getPrefix()}${COMMANDS.REMOVE_SERVER}`) {
+        //MESSAGE PROTOTYPE: !removeserver dmgttt
+
+        //1 argument must be provided
+        if (args.length != 1) { message.channel.send("Error: Invalid number of parameters"); return; }
+        message.channel.send("Deleting server...");
+
+        //extract info from arguments
+        let serverName = args[0];
+
+        let currentGuild = message.guild;
+        let currentGuildID = currentGuild.id;
+
+        //looks for server with given name in arguments
+        await Guild.findOne({ discordid: currentGuildID, "servers.name": serverName }, function (err, result) {
+            if (err) { console.error }
+            if (!result) { //if not found, sends error message
+                message.channel.send("Error: There is no server with that name. Try again.");
+                return;
+            } else { //if found:
+                //sends discord message warning that the server has been deleted.
+                message.channel.send("The server has been deleted from the database!");
+
+                //deletes text channel
+                let del = currentGuild.channels.find(e => e.name === serverName + "-player-list");
+                del.delete();
+
+                //deletes voice channel
+                var voiceChannelID;
+                Guild.findOne({ discordid: currentGuildID, "servers.name": serverName }).exec((err, data) => {
+                    let serverArray = data.servers;
+                    for (var i = 0; i < serverArray.length; i++) {
+                        if (serverArray[i].name == serverName) {
+                            voiceChannelID = serverArray[i].vchannelid
+                        }
+                    }
+                    currentGuild.channels.get(voiceChannelID).delete()
+                })
+
+                //updates database deleting the server in question
+                Guild.updateOne({ discordid: currentGuildID }, { $pull: { servers: { name: serverName } } })
+                    .then(console.log("Database updated"))
+                    .catch(console.error)
+
+            }
+        });
+    }
 }
+
+
 
 //Sets the "game" being played by the bot every 30 seconds
 bot.on("ready", () => {
